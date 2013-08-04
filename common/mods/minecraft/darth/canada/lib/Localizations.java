3d57766f84c9915dce1c55b0ad45c52113928de9
Darth_Feder <darth.feder.hi@gmail.com>
2013-08-04 11:05:16 -0400

    Changed over to assets.

--- common/mods/minecraft/darth/canada/lib/Localizations.java
+++ common/mods/minecraft/darth/canada/lib/Localizations.java
@@ -3,7 +3,7 @@ package mods.minecraft.darth.canada.lib;
 public class Localizations
 {
 
-    private static final String LANG_RESOURCE_LOCATION = "/mods/canada/lang/";
+    private static final String LANG_RESOURCE_LOCATION = "/assets/canada/lang/";
     
     public static String[] localeFiles = {LANG_RESOURCE_LOCATION + "en_US.xml"};
     
